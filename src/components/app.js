85a16007584dacd9fa24cb04c94456dfc0eb7f59
Lane <landave@acbc3284c2e1.ant.amazon.com>
2016-03-18 13:33:50 -0700

    react svg components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import styles from './app.css';
+import SampleIcon from './icons/sample-icon';
 
 export default class App extends Component {
   render() {
     return (
       <div className={styles.base}>
-        <h1>React Redux boilerplate with CSS Modules</h1>
+        <h1><SampleIcon /> React Redux boilerplate with CSS Modules</h1>
       </div>
     );
   }
